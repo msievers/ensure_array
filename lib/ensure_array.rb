fc7d2efcc97e7b82cd255d1a9977605abcc5ac49
Michael Sievers <michael_sievers@web.de>
2015-04-10 18:18:07 +0200

    Changed implementation slightly for code climate to see it

--- lib/ensure_array.rb
+++ lib/ensure_array.rb
@@ -1,11 +1,17 @@
 require "ensure_array/version"
 
-def EnsureArray(object, nil_value = [])
-  if object.is_a?(Array)
-    object
-  elsif object.nil?
-    nil_value
-  else
-    [object]
+module EnsureArray
+  def self.ensure_array(object, nil_value = [])
+    if object.is_a?(Array)
+      object
+    elsif object.nil?
+      nil_value
+    else
+      [object]
+    end
   end
+end
+
+def EnsureArray(*args)
+  EnsureArray.ensure_array(*args)
 end
